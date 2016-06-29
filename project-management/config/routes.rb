330e72d8c0816fd711469a6e27d79eb67baaba6c
Ryan <generyan.ebue@gmail.com>
2016-06-29 20:43:13 +1200

    add routes and delete old migration, add new migration that was created by devise

--- project-management/config/routes.rb
+++ project-management/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  resources :users
+  resources :users, only: [:index, :show, :edit, :update]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
