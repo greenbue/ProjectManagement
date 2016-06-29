b79e065adb6fa68e83189c9372d6b3988c6997a1
Ryan <generyan.ebue@gmail.com>
2016-06-29 20:42:24 +1200

    use devise to create users to ensure the security of the app
    make minor changes to the views and the css to make it look more "professional"

--- project-management/app/controllers/users_controller.rb
+++ project-management/app/controllers/users_controller.rb
@@ -1,26 +1,46 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update] # probably want to keep using this
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
   end
 
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      flash[:notice] = 'You signed up succefully!'
-    else
-      render 'new'
-    end
-    render @user
-  end
-
+  # # GET /users/1
+  # # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+
+  end
+
+  # GET /users/1/edit
+  def edit
+
+  end
+
+  # # PATCH/PUT /users/1
+  # # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
-  def user_params
-    params.require(:user).permit(:user_name, :email, :password)
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:role, :user_name)
+    end
 end
